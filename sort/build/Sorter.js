e9468ba3be97b95369532f5789ac212438173260
davide <info@davideravasi.com>
2022-04-26 12:12:11 +0000

    finished sort class

--- sort/build/Sorter.js
+++ sort/build/Sorter.js
@@ -1,16 +1,21 @@
 "use strict";
+// import { NumberCollection } from "./NumbersCollection";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Sorter = void 0;
+// interface Sortable {
+//   length(): number;
+//   swap(leftIndex: number, rightIndex: number): void;
+//   compare(leftIndex: number, rightIndex: number): boolean;
+// }
+// ts doesn't  know what length, compare and swap are
+// but we know that we never will use sorter alone
 class Sorter {
-    constructor(collection) {
-        this.collection = collection;
-    }
     sort() {
-        const collLength = this.collection.length();
+        const collLength = this.length();
         for (let i = 0; i < collLength; i++) {
             for (let j = 0; j < collLength - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
