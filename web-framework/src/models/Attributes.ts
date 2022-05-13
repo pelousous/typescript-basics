16bde6426e56bac5968447588111c224c71e2ced
davide <info@davideravasi.com>
2022-05-13 08:50:25 +0000

    added generics for the return attributes get class

--- web-framework/src/models/Attributes.ts
+++ web-framework/src/models/Attributes.ts
@@ -6,9 +6,17 @@ export class Attributes<T> {
   constructor(data: T) {
     this.data = data;
   }
-  get(userProp: string): string | number {
+  // instead of using th return union
+  // that oblige to write type guards when we want to use it
+  // we can create a type from the keys of the interface
+  // and use the return type inside of the object
+  // to determine the type returned T[K]
+  get<K extends keyof T>(userProp: K): T[K] {
     return this.data[userProp];
   }
+  // get(userProp: string): string | number {
+  //   return this.data[userProp];
+  // }
 
   set(userProps: T): void {
     Object.assign(this.data, userProps);
